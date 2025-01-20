0dbc6c90778f474dc8657f1f11367af14dcf677e
yash-letese <yashgaur@letese.co>
2025-01-20 19:37:36 +0530

    add: all files uploaded

---
+++ actions/account_opening.py
@@ -0,0 +1,13 @@
+# from typing import Any, Dict, List, Text
+
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.events import SlotSet
+# from rasa_sdk.executor import CollectingDispatcher
+
+# from actions.db import add_contact, get_contacts, Contact
+
+# class AddBankAccount(Action):
+#     def name(self) -> str:
+#         return "add_bank_account"
+
+#     def
